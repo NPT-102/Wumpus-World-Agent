3a63bbbf5b6e4a4a67012f3eb442b9b16a64fa9c
lad94220 <laduy231@clc.fitus.edu.vn>
2025-08-01 19:29:16 +0700

    place wumpus

--- env_simulator/generateMap.py
+++ env_simulator/generateMap.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-GOLD = 1
-
 class WumpusWorldGenerator:
   def __init__(self, N=8, wumpus=2, pits_probability=0.2):
     self.N = N
@@ -12,5 +10,15 @@ class WumpusWorldGenerator:
   def generate_map(self):
     # place gold
     gold_position = (np.random.randint(0, self.N), np.random.randint(0, self.N))
-    self.map[gold_position[0]][gold_position[1]] = GOLD
-    
+    self.map[gold_position[0]][gold_position[1]] = 'G'
+  
+    # place wumpus
+    wumpus = self.wumpus
+    for _ in range(wumpus):
+      while True:
+        wumpus_position = (np.random.randint(0, self.N), np.random.randint(0, self.N))
+        if self.map[wumpus_position[0]][wumpus_position[1]] is None:
+          if wumpus_position != gold_position:
+            if wumpus_position != (0, 0) and wumpus_position != (0, 1) and wumpus_position != (1, 0):
+              self.map[wumpus_position[0]][wumpus_position[1]] = 'W'
+              break
