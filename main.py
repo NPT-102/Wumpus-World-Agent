8ec64bb29db2b15ce8fa19dae3589ba36775dbb3
lad94220 <laduy231@clc.fitus.edu.vn>
2025-08-05 13:19:56 +0700

    example of kb usage

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from env_simulator.generateMap import WumpusWorldGenerator, print_map
+from env_simulator.kb import KnowledgeBase
 
 def main():
-  gen_map = WumpusWorldGenerator()
-  print("Generating Wumpus World Map...")
-  map = gen_map.generate_map()
-  print_map(map)
+  kb = KnowledgeBase(N=4)
+  kb.add_fact('~P(0, 0)')
+  kb.add_fact('~W(0, 0)')
+  kb.add_fact('~S(0, 0)')
+  kb.add_fact('~B(0, 0)')
+
+  kb.add_fact('B(0, 1)')
+  kb.add_fact('~S(0, 1)')
+  kb.forward_chain()
+    
+  kb.add_fact('S(1, 0)')
+  kb.add_fact('~B(1, 0)')
+  
+  kb.forward_chain() 
+  kb.print_knowledge()
   
 if __name__ == "__main__":
   main()
