9178c7d55e699068c6503544dfd6b248e5e8984c
stephrss <duythinhnguyen1703@gmail.com>
2025-08-10 19:48:15 +0700

    set initial background

--- visualization/UI.py
+++ visualization/UI.py
@@ -11,19 +11,21 @@ for row in range(GRID_SIZE):
     for col in range(GRID_SIZE):
         x1 = col * CELL_SIZE + CELL_SIZE
         y1 = row * CELL_SIZE
-        x2 = x1 + CELL_SIZE + CELL_SIZE
+        x2 = x1 + CELL_SIZE
         y2 = y1 + CELL_SIZE
 
-        color = 'lightgreen' if (row + col) % 2 == 0 else 'orange'
-        canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline='')
+        color = 'gray' if (row + col) % 2 == 0 else 'gray'
+        canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline='white')
 
 for i in range(GRID_SIZE):
-    xy = CELL_SIZE // 2
-    yy = i * CELL_SIZE + CELL_SIZE // 2
-    xx = (i + 1) * CELL_SIZE + CELL_SIZE // 2
-    yx = CELL_SIZE * GRID_SIZE + CELL_SIZE // 2
-    canvas.create_text(xy, yy, text=f'{GRID_SIZE - i - 1}')
-    canvas.create_text(xx, yx, text=f'{i}')
+    # xy = CELL_SIZE // 2
+    # yy = i * CELL_SIZE + CELL_SIZE // 2
+    # xx = (i + 1) * CELL_SIZE + CELL_SIZE // 2
+    # yx = CELL_SIZE * GRID_SIZE + CELL_SIZE // 2
+    # canvas.create_text(xy, yy, text=f'{GRID_SIZE - i - 1}')
+    # canvas.create_text(xx, yx, text=f'{i}')
+    canvas.create_text(CELL_SIZE//2, i*CELL_SIZE+CELL_SIZE//2, text=f'{GRID_SIZE - i - 1}')
+    canvas.create_text((i+1)*CELL_SIZE+CELL_SIZE//2, CELL_SIZE*GRID_SIZE+CELL_SIZE//2, text=f'{i}')
 
 
 
--- visualization/display.py
+++ visualization/display.py
@@ -3,17 +3,29 @@ from PIL import Image, ImageTk
 
 
 class Display(tk.Frame):
-    def __init__(self, parent, with_random, moving_wumpus):
+    def __init__(self, parent, with_random, moving_wumpus, grid_size, cell_size):
         super().__init__(parent)
         self.with_random = with_random
         self.moving_wumpus = moving_wumpus
-        if self.with_random:
-            pass
-            # color all cells with gray
-        else:
-            pass
-            # color safe cells with green, dangerous cells with red
+        self.grid = grid_size
+        self.cell = cell_size
+        self.canvas = tk.Canvas(parent, width=(self.grid+1)*self.cell, height=(self.grid+1)*self.cell)
+        self.canvas.pack()
+        self.set_initial_background()
     
+    def coordinate_converter():
+        pass
+    
+    def set_initial_background(self):
+        for row in range(self.grid):
+            for col in range(self.grid):
+                x = row * self.cell + self.cell
+                y = col * self.cell
+                self.canvas.create_rectangle(x, y, x+self.cell, y+self.cell, fill='gray', outline='white')
+        for i in range(self.grid):
+            self.canvas.create_text(self.cell//2, i*self.cell+self.cell//2, text=f'{self.grid - i - 1}')
+            self.canvas.create_text((i+1)*self.cell+self.cell//2, self.cell*self.grid+self.cell//2, text=f'{i}')
+
     def next(self):
         if self.with_random:
             pass
